f183912c3a90339221ab09e29927509d0ed2d974
Sanchez0823 <arturosanchezavocado23@gmail.com>
2025-02-12 21:25:52 -0600

    Improved auth.h code.

--- auth.h
+++ auth.h
@@ -1,16 +1,20 @@
 #ifndef AUTH_H
 #define AUTH_H
+//Defines the auth.h header file.
 
-#include <string>
-
+#include <string> // Allows the string variable
 using namespace std;
 
-class authenticateUser {
+class authenticateUser { 
+//Class authenticateUser with private and public declarations
 private:
+	// These private variable declaration contain the
+	// correct username and password.
 	string userName = "admin";
 	string password = "password123";
 
 public:
+	// Function declaration for authenticateUser
 	authenticateUser(string userNameInput, string passwordInput);
 };
 #endif //AUTH_H
