ebe6c9d2e18e8f04389d1afb8433863131aabbdc
Sanchez0823 <arturosanchezavocado23@gmail.com>
2025-02-12 21:38:09 -0600

    Update main.cpp file.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
-#include "auth.h"
+#include "auth.h" // Includes auth header file
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 int main() {
+    // Declares the following strings in the main.cpp
+    // so the authenticateUser function works.
     string userNameInput;
     string passwordInput;
+    // Boot up message.
     cout << "Booting up Basic OS...\n";
     cout << "Boot up process complete.\n";
+    // authenticateUser function and class call.
     authenticateUser authenticateUser(userNameInput, passwordInput);
-    return 0;
+    return 0; // Returns 0 when the program exits.
 }
