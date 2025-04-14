d220db392f25d609213e9d6fee62c259568869c3
Sanchez0823 <arturosanchezavocado23@gmail.com>
2025-04-13 23:38:52 -0500

    Included the header file for "virtual_memory.h"

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "auth.h" // Includes auth header file
+#include "virtual_memory.h" // new virtual memory file
 #include <iostream>
 #include <string>
 
@@ -12,8 +13,11 @@ int main() {
     // Boot up message.
     cout << "Booting up operating system...\n";
     cout << "Boot up process complete.\n";
-    cout << "---------- Welcome to Basic OS v0.1 ----------\a\n";
+    cout << "---------- Welcome to Basic OS v0.2 ----------\a\n";
     // authenticateUser function and class call.
     authenticateUser authenticateUser(userNameInput, passwordInput);
+
+    // virtual memory function
+
     return 0; // Returns 0 when the program exits.
 }
