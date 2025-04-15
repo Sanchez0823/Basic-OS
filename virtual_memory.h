8ca68718cdd6f04233d5b6803bdcf444b7b56179
Sanchez0823 <arturosanchezavocado23@gmail.com>
2025-04-15 08:28:57 -0500

    Updated virtual_memory.h

--- virtual_memory.h
+++ virtual_memory.h
@@ -3,7 +3,54 @@
 
 #include <iostream>
 #include <map>	// Used for implementing the page addresses
+#include <string>#ifndef VIRTUAL_MEMORY_H
+#define VIRTUAL_MEMORY_H
+
+#include <iostream>
+#include <map>	// Used for implementing the page addresses
 #include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std; // reduces the use of std::
+
+enum AccessMode {READ, WRITE};
+
+class pageTableAddress {
+public:
+	bool valid, dirty;
+	int frameNum;
+	
+	AccessMode accessMode; // page protection
+
+	pageTableAddress(bool valid, int frameNum, bool dirty, AccessMode accessMode)
+		: valid(valid), frameNum(frameNum), dirty(dirty), accessMode(accessMode) {}
+};
+
+// Renamed class virtualAddressSpace to virtualMemory for ease
+class virtualMemory {
+
+private:
+	int virtualMemorySize, physicalMemorySize; // memory size
+	int pageSize, numPages, numFrames; // frames and pages
+
+	map<int, vector<pageTableAddress>> processPageTables;
+	map<int, unordered_map<int, int>> processTLBs;
+
+public:
+
+	virtualMemory(int virtualMemorySize, int physicalMemorySize, int pageSize);
+	
+	int translateAddress(int pid, int virtualAddress, AccessMode mode);
+
+	// Process ID (pid)
+	void initializePageTable(int pid);
+	void displayPageTable(int pid) const;
+	void displayTLB(int pid) const;
+};
+
+#endif // !VIRTUAL_MEMORY_H
+
 #include <fstream>
 
 using namespace std; // reduces the use of std::
